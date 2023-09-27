737f39d2c02d54c661db7f2ca3fdcf28f752c4a2
lmjnels <lmjnelson@yahoo.com>
2023-09-27 20:03:28 +0100

    Add React and Typescript support

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                // 'resources/scss/style.scss', // need to add sass support
+                'resources/css/app.css',
+                'resources/js/app.tsx',
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+        },
+    },
+});
